ff4b57ab21bebc5e7040363875f25432405bc90c
LTH <johtta888@naver.com>
2022-05-28 17:16:15 +0900

    init

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      'styled-components',
+      {
+        filename: true,
+        displayname: true,
+        pure: true,
+      },
+    ],
+  ],
+};
